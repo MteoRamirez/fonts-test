cba27f0de731ca7882054263419a120a742b694a
mateo <eliermateoramirez@gmail.com>
2021-07-13 20:40:27 -0500

    fuentes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Main from './components/Main'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route>
+            <Main/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Main = (props) => {
+    return (
+        <Container>
+            <Content>
+                <p>This is a conection between the folder public and fonts</p>
+            </Content>
+        </Container>
+    )
+}
+
+const Container = styled.section`
+`
+const Content = styled.div``
+
+export default Main;
